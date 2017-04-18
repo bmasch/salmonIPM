7efefd4d77621fda93870b095349dee6dc55dfe2
Eric Buhle <eric.buhle@noaa.gov>
2017-04-17 17:39:14 -0700

    The package should now be fully operational

---
+++ IPM_PVA/test_script.R
@@ -0,0 +1,7 @@
+# load data
+fish_data <- read.table(file.path("~", "SalmonIPM", "tmp_data", "fish_data.txt"), sep = "\t", header = T)
+
+# fit hierarchical spawner-recruit model to run reconstruction
+test_fit <- salmonIPM(fish_data = fish_data, model = "RR", chains = 3, iter = 1000, warmup = 500)
+
+print(test_fit, pars = c("phi","R_hat"), include = F)
